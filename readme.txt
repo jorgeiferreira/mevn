30eca3555098b226d79c51401a74eb06b46e4f44
Jorge Ferreira <jorgeisaacferreira@gmail.com>
2020-07-29 11:25:37 -0400

    Mongoose, api version and babel first steps

---
+++ readme.txt
@@ -0,0 +1,2 @@
+#run mongo with docker
+docker run -d -p 27017-27019:27017-27019 --name mongodb mongo
---
+++ server/index.js
@@ -0,0 +1,16 @@
+
+import Express from 'express';
+import Mongoose from 'mongoose';
+
+import config from '@config';
+import v1Router from '@routes'
+
+Mongoose.connect(config.databaseUri, { useNewUrlParser: true });
+
+const app = Express();
+
+app.use(v1Router);
+
+app.listen(3000, () => {
+    console.log('server started yes');
+})

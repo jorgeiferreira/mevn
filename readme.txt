ce3325f8e3e3a2bb35d96a60f5901069e853e99b
Jorge Ferreira <jorgeisaacferreira@gmail.com>
2020-08-05 19:32:39 -0400

    Adding wepback middleware

--- readme.txt
+++ readme.txt
@@ -1,2 +1,5 @@
+#run app dev mode
+npm run dev
+
 #run mongo with docker
 docker run -d -p 27017-27019:27017-27019 --name mongodb mongo
--- server/index.js
+++ server/index.js
@@ -1,14 +1,26 @@
 import Express from 'express'
 import Mongoose from 'mongoose'
-
+import path from 'path'
 import config from '@config'
 import v1Router from '@routes'
+
+import WebpackConfig from '../webpack.config'
+import WebpackDevMiddleware from 'webpack-dev-middleware'
+import webpack from 'webpack'
 
 Mongoose.connect(config.databaseUri, { useNewUrlParser: true })
 
 const app = Express()
 
+const compiler = webpack(WebpackConfig)
+
+app.use(WebpackDevMiddleware(compiler))
+
 app.use(v1Router)
+
+app.use('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'))
+})
 
 app.listen(3000, () => {
     console.log('server started yes')

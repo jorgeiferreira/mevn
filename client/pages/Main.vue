a72a79c39378260b6ec6833d68f3be230ac04c3a
Jorge Ferreira <jorgeisaacferreira@gmail.com>
2020-08-20 21:25:58 -0400

    Sign up page design

--- client/pages/Main.vue
+++ client/pages/Main.vue
@@ -2,10 +2,10 @@
     <div>
         <div class="h-2 w-full bg-gold-light"></div>
         <div class="w-full h-12 flex items-center justify-between px-6" >
-            <div class="text-gold">Mevn</div>
+            <router-link to="/" class="no-underline text-gold">Mevn</router-link> 
             <div>
                 <router-link class="no-underline text-brown" to="/auth/login">Sign in</router-link>
-                <router-link class="no-underline text-brown border-2 p-1 rounded-full border-brown ml-3" to="/auth/register">Join now</router-link>
+                <router-link class="no-underline text-brown border-2 px-3 py-2 hover:text-brown-darkest rounded-full border-brown ml-3" to="/auth/register">Join now</router-link>
             </div>
         </div>
      
--- client/pages/Register.vue
+++ client/pages/Register.vue
@@ -1,5 +1,13 @@
 <template>
-    <div>
-        <h1>Sign up</h1>  
+    <div class="container my-16 w-full mx-auto">
+        <div class="max-w-sm mx-auto">
+            <h3 class="text-center text-gold text-3xl">Register</h3>  
+            <div class="w-full bg-white shadow mt-5 rounded-sm p-8">
+                <input type="text" class="text-sm w-full bg-brown-lightest p-3 focus:outline-none text-brown mb-3" placeholder="Your name">
+                <input type="text" class="text-sm w-full bg-brown-lightest p-3 focus:outline-none text-brown mb-3" placeholder="Your email">
+                <input type="text" class="text-sm w-full bg-brown-lightest p-3 focus:outline-none text-brown mb-3" placeholder="Your password">
+                <button class="w-full mt-3 py-4 bg-emerald text-white rounded-sm focus:outline-none hover:bg-emerald-light">Sign Up</button>
+            </div> 
+        </div> 
     </div>
 </template>

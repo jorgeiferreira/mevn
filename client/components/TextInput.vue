11e7c59954cfa0d3037f2b2cfc29340117767eec
Jorge Ferreira <jorgeisaacferreira@gmail.com>
2020-08-20 21:42:32 -0400

    Text input component with tailwin css

---
+++ client/components/TextInput.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+         <input 
+         :type="type"
+         :value="value" 
+         @input="$emit('input', $event.target.value)"
+         class="text-sm w-full bg-brown-lightest p-3 focus:outline-none text-brown mb-3" 
+         :placeholder="placeholder">
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        placeholder:{
+            type:String,
+            require:true,
+        },
+        type:{
+            type:String,
+            require:false,
+            default:'text'
+        },
+        value: {
+            type: String,
+            required:false,
+            default:''
+        }
+    }
+}
+</script>
--- client/pages/Register.vue
+++ client/pages/Register.vue
@@ -3,11 +3,23 @@
         <div class="max-w-sm mx-auto">
             <h3 class="text-center text-gold text-3xl">Register</h3>  
             <div class="w-full bg-white shadow mt-5 rounded-sm p-8">
-                <input type="text" class="text-sm w-full bg-brown-lightest p-3 focus:outline-none text-brown mb-3" placeholder="Your name">
-                <input type="text" class="text-sm w-full bg-brown-lightest p-3 focus:outline-none text-brown mb-3" placeholder="Your email">
-                <input type="text" class="text-sm w-full bg-brown-lightest p-3 focus:outline-none text-brown mb-3" placeholder="Your password">
+                <text-input v-model="model.name" placeholder="Your names"></text-input>
+                <text-input v-model="model.email" placeholder="Your email"></text-input>
+                <text-input v-model="model.password" placeholder="Your password" type="password"></text-input>
                 <button class="w-full mt-3 py-4 bg-emerald text-white rounded-sm focus:outline-none hover:bg-emerald-light">Sign Up</button>
             </div> 
         </div> 
     </div>
 </template>
+
+<script>
+export default {
+    data:()=>({
+        model:{
+          name:"",  
+          email:"",  
+          password:"",  
+        }
+    })
+}
+</script>

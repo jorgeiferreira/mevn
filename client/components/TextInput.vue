c3d0e47098d75cd1341de9eba72cf47ff3f2b242
Jorge Ferreira <jorgeisaacferreira@gmail.com>
2020-08-21 10:45:15 -0400

    Implement vee-validate

--- client/components/TextInput.vue
+++ client/components/TextInput.vue
@@ -1,11 +1,21 @@
 <template>
     <div>
-         <input 
-         :type="type"
-         :value="value" 
-         @input="$emit('input', $event.target.value)"
-         class="text-sm w-full bg-brown-lightest p-3 focus:outline-none text-brown mb-3" 
-         :placeholder="placeholder">
+        <ValidationProvider
+        :name="name"
+        :rules="rules"
+        v-slot="v">
+            <input 
+                :name="name"
+                :type="type"
+                :value="value" 
+                @input="$emit('input', $event.target.value)"
+                class="text-sm w-full bg-brown-lightest p-3 focus:outline-none text-brown mb-3" 
+                :placeholder="placeholder">
+            <span class="text-red">{{ v.errors[0] }}</span>
+        </ValidationProvider>
+
+     
+    
     </div>
 </template>
 <script>
@@ -24,7 +34,15 @@ export default {
             type: String,
             required:false,
             default:''
-        }
+        },
+        name: {
+            type: String,
+            required:true
+        },
+        rules: {
+            type: String,
+            required:true
+        }        
     }
 }
 </script>
--- client/index.js
+++ client/index.js
@@ -3,11 +3,31 @@ import './styles/main.css'
 import router from './routes'
 import Router from 'vue-router'
 import Main from '@pages/Main.vue'
+
+import { ValidationProvider, extend, ValidationObserver } from 'vee-validate';
+import { required, email } from 'vee-validate/dist/rules';
+
 import TextInput from '@components/TextInput.vue'
 
 Vue.use(Router)
 
+
 Vue.component('text-input',TextInput)
+Vue.component('ValidationProvider',ValidationProvider)
+Vue.component('ValidationObserver',ValidationObserver )
+
+
+// Add the required rule
+extend('required', {
+    ...required,
+    message: 'This field is required'
+  });
+   
+  // Add the email rule
+  extend('email', {
+    ...email,
+    message: 'This field must be a valid email'
+  });
 
 const app = new Vue({
     el: '#app',

506018fff801ac37d4ab717c45c62394401e4fda
Jorge Ferreira <jorgeisaacferreira@gmail.com>
2020-08-25 01:07:55 -0400

    encapsulate validation

--- client/index.js
+++ client/index.js
@@ -5,36 +5,16 @@ import Router from 'vue-router'
 import Main from '@pages/Main.vue'
 import TextInput from '@components/TextInput.vue'
 import store from '@store'
-
-
-import { ValidationProvider, extend, ValidationObserver } from 'vee-validate';
-import { required, email } from 'vee-validate/dist/rules';
+import validation from './validation'
 
 
 Vue.component('text-input',TextInput)
-Vue.component('ValidationProvider',ValidationProvider)
-Vue.component('ValidationObserver',ValidationObserver )
-
-
-// Add the required rule
-extend('required', {
-    ...required,
-    message: 'This field is required'
-  });
-   
-  // Add the email rule
-  extend('email', {
-    ...email,
-    message: 'This field must be a valid email'
-  });
-
-
 Vue.use(Router)
-
 
 const app = new Vue({
     el: '#app',
     router,
     store,
+    validation,
     render: createElement => createElement(Main)
 })
---
+++ client/validation/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+
+import { ValidationProvider, extend, ValidationObserver } from 'vee-validate';
+import { required, email } from 'vee-validate/dist/rules';
+
+Vue.component('ValidationProvider',ValidationProvider)
+Vue.component('ValidationObserver',ValidationObserver )
+
+// Add the required rule
+extend('required', {
+    ...required,
+    message: 'This field is required'
+});
+
+// Add the email rule
+extend('email', {
+    ...email,
+    message: 'This field must be a valid email'
+});

897783ba86fb3cd15b85ca1d7ae0934759588294
Jorge Ferreira <jorgeisaacferreira@gmail.com>
2020-08-28 08:54:27 -0400

    Reset link validation

--- server/routes/v1/auth.js
+++ server/routes/v1/auth.js
@@ -6,10 +6,13 @@ import registerValidator from '@validators/register'
 
 import loginValidator from '@validators/login'
 
+import forgotPasswordValidator from '@validators/forgot-password'
+
 const authRouter = new Router()
 
 authRouter.post('/login', loginValidator, authController.login)
 authRouter.post('/register', registerValidator, authController.register)
-authRouter.post('/passwords/email', authController.forgotPassword)
+authRouter.post('/passwords/email', forgotPasswordValidator, authController.forgotPassword)
 
 export default authRouter
+
